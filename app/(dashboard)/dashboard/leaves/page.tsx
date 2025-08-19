833b5c687ec7a78419bd53983de4a8e070c27cf8
Fathur Rahman <cyberflyx@gmail.com>
2025-08-19 16:42:00 +0800

    fetch users leaves with server function

--- app/(dashboard)/dashboard/leaves/page.tsx
+++ app/(dashboard)/dashboard/leaves/page.tsx
@@ -1,6 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getUserLeaves } from "@/lib/actions/leave_actions";
 
-export default function LeavesPage() {
+export default async function LeavesPage() {
+  const user_leaves = await getUserLeaves();
+
+  console.log("User Leaves:", user_leaves);
+
   return (
     <>
       <h3 className="mb-8 text-2xl font-bold">Manage Leaves</h3>
---
+++ lib/actions/leave_actions.ts
@@ -0,0 +1,20 @@
+import { desc, and, eq, isNull } from 'drizzle-orm';
+import { db } from '@/lib/db/drizzle';
+import { users, leave_applications } from '@/lib/db/schema';
+import { getUser } from "@/lib/db/queries";
+
+export async function getUserLeaves() {
+  console.log("Fetching user leaves...");
+
+  const user = await getUser();
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const result = await db
+      .select()
+      .from(leave_applications)
+      .where(eq(leave_applications.userId, user.id));
+  
+    return result.length > 0 ? result[0] : null;
+}
