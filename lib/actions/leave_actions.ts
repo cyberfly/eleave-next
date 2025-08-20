37e0e059d6478f1c10cbfee6a43ed9c392f67793
Fathur Rahman <cyberflyx@gmail.com>
2025-08-20 15:50:53 +0800

    leave application form validation with zod

--- lib/actions/leave_actions.ts
+++ lib/actions/leave_actions.ts
@@ -4,8 +4,16 @@ import { desc, and, eq, isNull } from "drizzle-orm";
 import { db } from "@/lib/db/drizzle";
 import { users, leave_applications } from "@/lib/db/schema";
 import { getUser } from "@/lib/db/queries";
-import { redirect } from 'next/navigation'
-import { revalidatePath } from 'next/cache'
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import * as z from "zod";
+
+const LeaveApplicationFormRequest = z.object({
+  leave_type: z.string().min(1, "Leave type is required"),
+  description: z.string().min(1, "Description is required"),
+  start_date: z.string().date().min(1, "Start Date is required"),
+  end_date: z.string().date().min(1, "End Date is required"),
+});
 
 export async function getUserLeaves() {
   console.log("Fetching user leaves...");
@@ -41,16 +49,25 @@ export async function storeLeaveApplication(formData: FormData) {
     description: description,
     start_date: start_date,
     end_date: end_date,
-    userId: user.id
+    userId: user.id,
   };
+
+  //   validate form data
+
+  const validate_result = LeaveApplicationFormRequest.safeParse(leave_data);
+
+  if (!validate_result.success) {
+    console.error("Validation errors:", validate_result.error.errors);
+    return;
+  }
 
   const latest_leave = await db
     .insert(leave_applications)
     .values(leave_data)
     .returning();
 
-   console.log("Leave application stored:", latest_leave);
+  console.log("Leave application stored:", latest_leave);
 
-   revalidatePath('/dashboard/leaves')
-   redirect('/dashboard/leaves')
+  revalidatePath("/dashboard/leaves");
+  redirect("/dashboard/leaves");
 }
