986b9dfdeea192622fbe83c71287e1b348468ba0
Fathur Rahman <cyberflyx@gmail.com>
2025-08-21 09:19:18 +0800

    fix table missing thead tbody key

--- app/(dashboard)/dashboard/leaves/page.tsx
+++ app/(dashboard)/dashboard/leaves/page.tsx
@@ -1,4 +1,3 @@
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getUserLeaves } from "@/lib/actions/leave_actions";
 
@@ -16,34 +15,32 @@ export default async function LeavesPage() {
           <CardTitle>Your Leave Applications</CardTitle>
         </CardHeader>
         <CardContent>
-
-        <table className="table">
-            <tr>
+          <table className="table">
+            <thead>
+              <tr>
                 <th>Description</th>
                 <th>Type</th>
                 <th>Start Date</th>
                 <th>End Date</th>
                 <th>Status</th>
                 <th>Action</th>
-            </tr>
+              </tr>
+            </thead>
 
-            {user_leaves.map((leave_row, index) => ( 
-
-            <tr>
-                <td>{leave_row.description}</td>
-                <td>{leave_row.leave_type}</td>
-                <td>{leave_row.start_date}</td>
-                <td>{leave_row.end_date}</td>
-                <td>{leave_row.status}</td>
-                <td>
-
-                </td>
-            </tr>
-
-            ))}
-
-        </table>
-
+            <tbody>
+              {user_leaves &&
+                user_leaves.map((leave_row, index) => (
+                  <tr key={index}>
+                    <td>{leave_row.description}</td>
+                    <td>{leave_row.leave_type}</td>
+                    <td>{leave_row.start_date}</td>
+                    <td>{leave_row.end_date}</td>
+                    <td>{leave_row.status}</td>
+                    <td></td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </CardContent>
       </Card>
     </>
